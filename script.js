47ac8bf867afbe7a6803fa09c45d32543921d7ca
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-08 21:15:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let prices = document.getElementsByClassName("price");
+	let sum =0 ;
+	console.log(prices);
+	Array.from(prices).forEach((el)=>console.log(el.innerText))
+	sum=Array.from(prices).reduce((acc,curr)=>acc+=Number(curr.innerText),0);
+	let tr=document.createElement("tr");
+	let td= document.createElement("td");
+	td.innerText=sum;
+	tr.appendChild(td);
+	let table =document.getElementById("table");
+	table.appendChild(tr)
   
 };
 
